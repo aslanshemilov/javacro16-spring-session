e5db2309aa27b51fd898e4b5edbf4d7e4f1abb1c
Vedran Pavic <vedran.pavic@gmail.com>
2016-05-13 10:12:41 +0200

    Add basic-hazelcast demo

---
+++ basic-hazelcast/src/main/java/hr/kapsch/javacro/session/BasicHazelcastSessionApplication.java
@@ -0,0 +1,22 @@
+package hr.kapsch.javacro.session;
+
+import com.hazelcast.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+@SpringBootApplication
+@EnableHazelcastHttpSession
+public class BasicHazelcastSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicHazelcastSessionApplication.class, args);
+	}
+
+	@Bean
+	Config config() {
+		return new Config();
+	}
+
+}
---
+++ basic-hazelcast/src/main/java/hr/kapsch/javacro/session/DemoController.java
@@ -0,0 +1,25 @@
+package hr.kapsch.javacro.session;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	private static final String TEMPLATE =
+			"<table border=\"1\">" +
+			"<tr><th>id</th><th>creation time</th><th>last accessed time</th></tr>" +
+			"<tr><td>%s</td><td>%s</td><td>%s</td></tr>" +
+			"</table>";
+
+	@GetMapping("/")
+	String sessionInfo(HttpSession session) {
+		return String.format(TEMPLATE, session.getId(),
+				new Date(session.getCreationTime()), new Date(session.getLastAccessedTime()));
+	}
+
+}
